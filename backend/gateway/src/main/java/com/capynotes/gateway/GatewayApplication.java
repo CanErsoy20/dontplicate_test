afc1289988ce771ff83b3f0752f9f5f795b2e626
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2024-04-07 15:26:25 +0300

    test comment

--- backend/gateway/src/main/java/com/capynotes/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/com/capynotes/gateway/GatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
-
+	// This is a comment
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
